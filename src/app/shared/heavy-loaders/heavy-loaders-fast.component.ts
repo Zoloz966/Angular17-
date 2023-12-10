21c2c93cf9a35f8f334bed907cc8cfc468705a19
Zolouz966 <zrodrigo96@gmail.com>
2023-12-09 23:14:36 -0400

    Add @Defer on interaction in defer options component

--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,15 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
   imports: [CommonModule],
-  template: `<h1>Hola mundo</h1>`,
-  styles: `
-    :host {
-      display: block;
-    }
+  template: `
+    <section [ngClass]="['w-full', cssClass]">
+      <ng-content />
+    </section>
   `,
 })
-export class HeavyLoadersFastComponent {}
+export class HeavyLoadersFastComponent {
+  @Input({ required: true }) cssClass!: string;
+
+  constructor(){
+    console.log('Creado HLF');
+
+  }
+
+}
